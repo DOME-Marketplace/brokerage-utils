9b6edd11547fa3679e58dfce81016cf5a84e16f5
pasquy <pasquale.vitale@gmail.com>
2025-10-01 11:49:48 +0200

    add customer management

--- src/main/java/it/eng/dome/brokerage/api/CustomerManagementApis.java
+++ src/main/java/it/eng/dome/brokerage/api/CustomerManagementApis.java
@@ -1,12 +1,12 @@
 package it.eng.dome.brokerage.api;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import it.eng.dome.brokerage.api.page.Page;
 import it.eng.dome.tmforum.tmf629.v4.ApiClient;
 import it.eng.dome.tmforum.tmf629.v4.ApiException;
 import it.eng.dome.tmforum.tmf629.v4.api.CustomerApi;
@@ -18,8 +18,6 @@ import it.eng.dome.tmforum.tmf629.v4.model.CustomerUpdate;
 public class CustomerManagementApis {
 
 	private final Logger logger = LoggerFactory.getLogger(CustomerManagementApis.class);
-	private final int LIMIT = 100;
-
 	private CustomerApi customerApi;
 
 	/**
@@ -39,7 +37,9 @@ public class CustomerManagementApis {
 	 * @param CustomerCreate - CustomerCreate object used in the creation request of the Customer (required) 
 	 * @return Customer
 	 */
-	public Customer createCustomer(CustomerCreate customerCreate) {		
+	public Customer createCustomer(CustomerCreate customerCreate) {	
+		logger.info("Create: Customer");
+		
 		try {
 			return customerApi.createCustomer(customerCreate);
 		} catch (ApiException e) {
@@ -57,16 +57,18 @@ public class CustomerManagementApis {
 	 * @return boolean
 	 */
 	public boolean updateCustomer(String customerId, CustomerUpdate customerUpdate) {
-		logger.info("Request: updateCustomer");
+		logger.info("Request: updateCustomer by id {}", customerId);
+		
 		try {
 			Customer customer = customerApi.patchCustomer(customerId, customerUpdate);
-			logger.info("Update Customer with id: {}", customer.getId());
+			logger.info("Update successfully Customer with id: {}", customer.getId());
 			return true;
 		} catch (ApiException e) {
 			logger.error("Error: {}", e.getResponseBody());
 			return false;
 		}
 	}
+	
 
 	/**
 	 * This method retrieves a specific Customer by ID
@@ -78,7 +80,13 @@ public class CustomerManagementApis {
 	 * @return Customer
 	 */
 	public Customer getCustomer(String customerId, String fields) {
+		logger.info("Request: getCustomer by id {}", customerId);
+		
 		try {
+			if (fields != null) {
+				logger.debug("Selected attributes: [{}]", fields);
+			}
+			
 			return customerApi.retrieveCustomer(customerId, fields);
 		} catch (ApiException e) {
 			logger.error("Error: {}", e.getResponseBody());
@@ -86,44 +94,39 @@ public class CustomerManagementApis {
 		}
 	}
 	
+	
 	/**
-	 * This method retrieves the list of UsCustomerage
+	 * This method retrieves a paginated list of Customer
 	 * 
 	 * @param fields - Comma-separated properties to be provided in response (optional)<br> 
-	 * - use this string to get specific fields (separated by comma: i.e. 'status,name')<br> 
-	 * - use fields == null to get all attributes
-	 * @param filter - HashMap<K,V> to set query string params (optional)<br> 
-	 * @return List&lt;Customer&gt;
+	 * - use this string to get specific fields (separated by comma: i.e. 'name,status')<br>
+	 * - use fields == null to get all attributes		
+     * @param offset - the index of the first item to return (used for pagination)
+     * @param limit - the maximum number of items to return
+	 * @param filter - HashMap<K,V> to set query string params (optional)<br>  
+	 * @return a {@link Page} containing a subset of Customer
 	 */
-	public List<Customer> getAllCustomers(String fields, Map<String, String> filter) {
-		logger.info("Request: getAllCustomers");
-		List<Customer> all = new ArrayList<Customer>();
+	public Page<Customer> listCustomers(String fields, int offset, int limit, Map<String, String> filter) {
+		logger.info("Request: listCustomers");
 		
-		if (filter != null && !filter.isEmpty()) {
-			logger.debug("Params used in the query-string filter: {}", filter);
-		}
-		
-		getAllCustomers(all, fields, 0, filter);
-		logger.info("Number of Customers: {}", all.size());
-		return all;
-	}
-		
-	private void getAllCustomers(List<Customer> list, String fields, int start, Map<String, String> filter) {
-		int offset = start * LIMIT;
-
 		try {
-			List<Customer> customerList =  customerApi.listCustomer(fields, offset, LIMIT, filter);
-
-			if (!customerList.isEmpty()) {
-				list.addAll(customerList);
-				getAllCustomers(list, fields, start + 1, filter);				
-			}else {
-				return;
+			
+			if (filter != null && !filter.isEmpty()) {
+				logger.debug("Params used in the query-string filter: {}", filter);
 			}
+			if (fields != null) {
+				logger.debug("Selected attributes: [{}]", fields);
+			}
+			
+			List<Customer> items = customerApi.listCustomer(fields, offset, limit, filter);
+			boolean hasNext = items.size() == limit;
+			
+			return new Page<>(items, offset, limit, hasNext);
+			
 		} catch (ApiException e) {
 			logger.error("Error: {}", e.getResponseBody());
-			return;
-		}		
+			return null;
+		}   
 	}
-	
+		
 }
