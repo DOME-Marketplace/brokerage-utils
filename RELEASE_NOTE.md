10b68090a965d6d18ae948768e0714c792e45ad4
pasquy <pasquale.vitale@gmail.com>
2025-11-14 20:13:35 +0100

    handling non-compliant objects

--- RELEASE_NOTE.md
+++ RELEASE_NOTE.md
@@ -2,9 +2,14 @@
 
 **Release Notes** of the *Brokerage Utils* software:
 
+### <code>2.2.3</code> :calendar: 14/11/2025
+**BugFix**
+* Fixed handling of **non-compliant** objects in `FetchUtils` by applying a **divide-and-conquer** fallback in `safeFetchRange` method, ensuring valid items are retained.
+
+
 ### <code>2.2.2</code> :calendar: 30/10/2025
 **BugFix**
-* Update `ProductOfferingPriceUtils` to retrieve all fields of `ProductOfferingPrice`
+* Update `ProductOfferingPriceUtils` to retrieve all fields of `ProductOfferingPrice`.
 
 ### <code>2.2.1</code> :calendar: 24/10/2025
 **Improvements**
--- src/test/java/it/eng/dome/brokerage/test/ProductCatalogManagementApisTest.java
+++ src/test/java/it/eng/dome/brokerage/test/ProductCatalogManagementApisTest.java
@@ -1,6 +1,9 @@
 package it.eng.dome.brokerage.test;
 
+import java.net.URI;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,12 +19,13 @@ import it.eng.dome.tmforum.tmf620.v4.model.ProductOfferingCreate;
 import it.eng.dome.tmforum.tmf620.v4.model.ProductOfferingPrice;
 import it.eng.dome.tmforum.tmf620.v4.model.ProductOfferingUpdate;
 import it.eng.dome.tmforum.tmf620.v4.model.ProductSpecification;
+import it.eng.dome.tmforum.tmf620.v4.model.RelatedParty;
 import it.eng.dome.tmforum.tmf620.v4.model.TimePeriod;
 
 public class ProductCatalogManagementApisTest {
 
 	final static String tmf620ProductCatalogPath = "tmf-api/productCatalogManagement/v4";
-	final static String tmfEndpoint = "https://tmf.dome-marketplace-sbx.org";
+	final static String tmfEndpoint = /*"https://dome-dev.eng.it"; */ "https://tmf.dome-marketplace-sbx.org";
 	
 
 	public static void main(String[] args) {
@@ -31,9 +35,10 @@ public class ProductCatalogManagementApisTest {
 		 * ProductOffering
 		 */
 //		TestCreateProductOffering();
-//		TestGetAllProductOffering();
+		TestGetAllProductOffering();
+//		TestGetAllProductOfferingFetchByBatch();
 //		TestGetFilteredProductOffering();		
-//		String id = "urn:ngsi-ld:product-offering:d2d6e74e-1d18-478f-a435-6b56e6f44dd3";
+//		String id = "urn:ngsi-ld:product-offering:ac2d9699-2b55-402d-8028-c229b9683dba";
 //		TestGetProductOffering(id);		
 //		TestUpdateProductOffering(id);
 		
@@ -56,7 +61,7 @@ public class ProductCatalogManagementApisTest {
 		/**
 		 * Category
 		 */
-		TestGetAllCategories();
+//		TestGetAllCategories();
 //		String id = "urn:ngsi-ld:category:ace3054c-a354-44e8-b8e3-b47966211cc7";
 //		TestGetCategory(id);
 		
@@ -89,8 +94,24 @@ public class ProductCatalogManagementApisTest {
 		tp.setEndDateTime(OffsetDateTime.now().plusDays(10));
 		poc.setValidFor(tp);
 		
+		RelatedParty rp = new RelatedParty();
+		rp.setAtReferredType("Organization");
+		rp.setHref(URI.create("urn:ngsi-ld:organization:eb6647da-84f2-4645-8d9f-c2905775b561"));
+		rp.setId("urn:ngsi-ld:organization:eb6647da-84f2-4645-8d9f-c2905775b561");
+		rp.setName("did:elsi:VATIT-12622480155");
+		rp.setRole("Seller");
+		
+		List<RelatedParty> parties = new ArrayList<RelatedParty>();
+		parties.add(rp);
+		poc.setRelatedParty(parties);
+		String schemaLocation = "https://raw.githubusercontent.com/DOME-Marketplace/tmf-api/refs/heads/main/DOME/ShareableEntity.schema.json"; //OK
+		//String schemaLocation = "https://raw.githubusercontent.com/Sh3rd3n/SchemaValidationTest/refs/heads/main/ProductOfferingExtension.schema.json"; //OK creazione -> no array
+		//String schemaLocation = "https://raw.githubusercontent.com/pasquy73/test-workflow/refs/heads/test_related/ProductOfferingExtensionTest.schema.json"; //OK
+		poc.setAtSchemaLocation(URI.create(schemaLocation));
+		
 		String id = null;
 		try {
+			System.out.println(poc.toJson());
 			id = apis.createProductOffering(poc);
 		} catch (ApiException e) {
 			// TODO Auto-generated catch block
@@ -112,14 +133,52 @@ public class ProductCatalogManagementApisTest {
 	        apis::listProductOfferings,		// method reference
 	        null,                       	// fields
 	        null, 				    		// filter
-	        100                         	// pageSize
+	        10                         	// pageSize
 		) 
 		.forEach(po -> { 
 			count.incrementAndGet();
-			System.out.println(count + " " + po.getId() + " → " + po.getName() + " / " + po.getLifecycleStatus());
+			System.out.print(count + " " + po.getId() + " → " + po.getName() + " / " + po.getLifecycleStatus() + " / ");
+
+			if (po.getRelatedParty() != null) {
+				System.out.println(po.getRelatedParty().get(0).getName());
+			}else {
+				System.out.println("no relatedParty");
 			}
+
+		}
 		);		
 		
+		System.out.println("ProductOffering found: " + count);
+	}
+	
+	protected static void TestGetAllProductOfferingFetchByBatch() {
+
+		ApiClient apiClientTmf620 = Configuration.getDefaultApiClient();
+		apiClientTmf620.setBasePath(tmfEndpoint + "/" + tmf620ProductCatalogPath);
+
+		ProductCatalogManagementApis apis = new ProductCatalogManagementApis(apiClientTmf620);		
+		AtomicInteger count = new AtomicInteger(0);
+		
+		FetchUtils.fetchByBatch(
+			apis::listProductOfferings, 
+			null, 
+			null, 
+			10,
+			batch -> {
+			    batch.forEach(po -> {
+			    	count.incrementAndGet();
+			    	System.out.print(count + " " + po.getId() + " → " + po.getName() + " / " + po.getLifecycleStatus() + " / ");
+
+					if (po.getRelatedParty() != null) {
+						System.out.println(po.getRelatedParty().get(0).getName());
+					}else {
+						System.out.println("no relatedParty");
+					}
+
+			    });
+			}
+		);
+				
 		System.out.println("ProductOffering found: " + count);
 	}
 	
@@ -173,7 +232,7 @@ public class ProductCatalogManagementApisTest {
 		try {
 			ProductOffering po = apis.getProductOffering(id, null);
 			if (po != null) {
-				System.out.println(po.getId() + " " + po.getName() + " " + po.getLifecycleStatus());
+				System.out.println(po.getId() + " " + po.getName() + " " + po.getRelatedParty().size());
 			}
 		} catch (ApiException e) {
 			System.err.println("Error: " + e.getMessage());
@@ -193,7 +252,7 @@ public class ProductCatalogManagementApisTest {
 	        apis::listProductOfferingPrices,	// method reference
 	        null,                       		// fields
 	        null, 				    			// filter
-	        100                         		// pageSize
+	        10                         		// pageSize
 		) 
 		.forEach(pop -> { 
 			count.incrementAndGet();
@@ -323,7 +382,12 @@ public class ProductCatalogManagementApisTest {
 		.forEach(c -> { 
 			count.incrementAndGet();
 			System.out.println(count + " " + c.getId() + " → " + c.getName() + " / " + c.getLifecycleStatus());
+			if (c.getRelatedParty() != null) {
+				System.out.println(c.getRelatedParty().size());
+			}else {
+				System.out.println("no relatedParty");
 			}
+		}
 		);		
 		
 		System.out.println("Catlogs found: " + count);
